e914f642c222f65fbc5bb04b5eb91aa79f585ebf
Rohit Rajsurya Prasad <rohit.rajsurya10@gmail.com>
2024-05-14 14:51:01 +0530

    adding deploy scripts

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,24 +1,20 @@
-import { config } from "dotenv";
+import { HardhatUserConfig } from "hardhat/config";
 
-config({
-  path: '.env'
-})
-
-import { HardhatUserConfig } from "hardhat/types";
-
-import 'hardhat-abi-exporter';
+import "hardhat-deploy";
+// To make hardhat-waffle compatible with hardhat-deploy
+// we have aliased hardhat-ethers to hardhat-ethers-deploy in package.json
 import "@nomiclabs/hardhat-waffle";
-import "@nomiclabs/hardhat-etherscan";
+import "hardhat-gas-reporter";
 import "@typechain/hardhat";
 import "solidity-coverage";
-import "hardhat-gas-reporter";
-
-const ACCOUNT_PRIVATE_KEY = process.env.ACCOUNT_PRIVATE_KEY || "";
-const ARBSCAN_API_KEY = process.env.ARBSCAN_API_KEY || "";
-const ARB_GOERLI_API_KEY = process.env.ARB_GOERLI_API_KEY || "";
+const PRIVATE_KEY = process.env.PRIVATE_KEY || "0x21509d66752f8a9f01333d0002cecfb6d915a5a0be5cd54fb095b7b31321f518";
 
 const hardhatConfig: HardhatUserConfig = {
-  defaultNetwork: "hardhat",
+  defaultNetwork: "berachain",
+  namedAccounts: {
+    deployer: 0, // Do not use this account for testing
+    admin: 1,
+},
   solidity: {
     compilers: [
       {
@@ -34,25 +30,18 @@ const hardhatConfig: HardhatUserConfig = {
   },
   networks: {
     hardhat: {},
-    arbitrumTestnet: {
-      url: `https://arb-goerli.g.alchemy.com/v2/${ARB_GOERLI_API_KEY}`,
-      gasPrice: 1000000000,
-      accounts: [ACCOUNT_PRIVATE_KEY],
-    },
+    berachain: { 
+      url: `https://artio.rpc.berachain.com/`,  
+      accounts: [PRIVATE_KEY]
+   },
     coverage: {
       url: "http://127.0.0.1:8555", // Coverage launches its own ganache-cli client
     },
   },
-  etherscan: {
-    apiKey: {
-      // For all arbi networks
-      arbitrumTestnet: ARBSCAN_API_KEY,
-    }
-  },
-  gasReporter: {
-    currency: 'USDT',
-    coinmarketcap: process.env.COINMARKETCAP_API,
-  },
+  // gasReporter: {
+  //   currency: 'USDT',
+  //   coinmarketcap: process.env.COINMARKETCAP_API,
+  // },
 };
 
 export default hardhatConfig;
