fa73ea367722769640e4c16ca923c14a843cf934
Rohit Rajsurya Prasad <rohit.rajsurya10@gmail.com>
2024-05-16 21:41:20 +0530

    updating deploy script

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -48,6 +48,57 @@ async function main() {
    await coinFlip.deployed();
 
    console.log(`Coin Flip game deployed to ${coinFlip.address}`);
+   //2. Dice
+   const Dice = await hre.ethers.getContractFactory("Dice");
+   const dice = await Dice.deploy(randomizerRouter.address);
+
+   await dice.deployed();
+
+   console.log(`Dice game deployed to ${dice.address}`);
+   //3. Limbo
+   const Limbo = await hre.ethers.getContractFactory("Limbo");
+   const limbo = await Limbo.deploy(randomizerRouter.address);
+
+   await limbo.deployed();
+
+   console.log(`Limbo game deployed to ${limbo.address}`);
+   //4. Plinko
+   const Plinko = await hre.ethers.getContractFactory("Plinko");
+   const plinko = await CoinFlip.deploy(randomizerRouter.address);
+
+   await plinko.deployed();
+
+   console.log(`Plinko game deployed to ${plinko.address}`);
+   //5. Range
+   const Range = await hre.ethers.getContractFactory("Range");
+   const range = await Range.deploy(randomizerRouter.address);
+
+   await range.deployed();
+
+   console.log(`Range game deployed to ${range.address}`);
+   //6. RPS
+   const RPS = await hre.ethers.getContractFactory("RPS");
+   const rPS = await RPS.deploy(randomizerRouter.address);
+
+   await rPS.deployed();
+
+   console.log(`Coin Flip game deployed to ${rPS.address}`);
+   //7. Moon - Multiplayer
+   const Moon = await hre.ethers.getContractFactory("Moon");
+   const moon = await Moon.deploy(randomizerRouter.address);
+
+   await moon.deployed();
+
+   console.log(`Moon game deployed to ${moon.address}`);
+
+   //8. Wheel
+   const Wheel = await hre.ethers.getContractFactory("Wheel");
+   const wheel = await Wheel.deploy(randomizerRouter.address);
+
+   await wheel.deployed();
+
+   console.log(`Wheel game deployed to ${wheel.address}`);
+
 
 
 
